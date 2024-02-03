5f52f92af299049e7cf4b3f9139d486ce81ba99a
RogerCorb <rogeriocgprest@gmail.com>
2024-02-02 22:48:07 -0300

    criado pagina contato e app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const envio=() => {   
+  const nome = document.querySelector('[name="nome"]').value;
+  const email = document.querySelector('[name="email"]').value;
+
+  console.log(nome);
+  console.log(email);
+
+  // Aqui você pode processar os dados (por exemplo, enviá-los por e-mail)
+  // ou fazer outras ações necessárias.  
+}
