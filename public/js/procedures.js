22200f8ae8d21db3e2f23c9a9ed464665d35861b
RogerCorb <rogeriocgprest@gmail.com>
2024-02-04 18:28:58 -0300

    arquivo procedures.js adicionado

---
+++ public/js/procedures.js
@@ -0,0 +1,25 @@
+
+function Keepgo() {
+   const nome = document.querySelector('[name="nome"]').value;
+   const email = document.querySelector('[name="email"]').value;
+   const subject = document.querySelector('[name="subject"]').value;
+   const body= document.querySelector('[name="body"]').value;
+
+   console.log(nome);
+   console.log(email);
+}
+
+/*
+const botao = document.querySelector('#meuBotao');
+botao.addEventListener('click', function() {
+   // Código a ser executado quando o botão é clicado
+   const nome = document.querySelector('[name="nome"]').value;
+   const email = document.querySelector('[name="email"]').value;
+   const subject = document.querySelector('[name="subject"]').value;
+   const body= document.querySelector('[name="body"]').value;
+
+   console.log(nome);
+   console.log(email);
+   console.log('Botão clicado!');
+});
+*/
