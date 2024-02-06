5479bc095b8ed8c4d2725473bb7612920d369ea8
RogerCorb <rogeriocgprest@gmail.com>
2024-02-06 11:50:29 -0300

    ajustes na pagina contato

--- public/js/procedures.js
+++ public/js/procedures.js
@@ -1,4 +1,6 @@
 
+
+
 function Keepgo() {
     
     const nome = document.querySelector('[name="nome"]').value;
@@ -8,3 +10,25 @@ function Keepgo() {
     
     console.log(nome);
 }
+
+
+/*const form = document.getElementById('myForm');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // Impede o envio padrão do formulário
+
+  // Aqui você pode acessar os valores dos campos do formulário
+  const nome = document.querySelector('[name="nome"]').value;
+  const email = document.querySelector('[name="email"]').value;
+  const assunto = document.querySelector('[name="subject"]').value;
+  const descricao= document.querySelector('[name="body"]').value; 
+    
+ 
+
+  // Faça o que desejar com os dados (por exemplo, enviar para o servidor)
+  console.log('Nome:', nome);
+  console.log('E-mail:', email);
+});
+
+*/
+
