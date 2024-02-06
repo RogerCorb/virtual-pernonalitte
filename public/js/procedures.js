148b94bfa0131008e937856a3caf3f32b7685b4d
RogerCorb <rogeriocgprest@gmail.com>
2024-02-06 08:11:03 -0300

    Atualizacao funçao keepgo

--- public/js/procedures.js
+++ public/js/procedures.js
@@ -1,26 +1,10 @@
 
 function Keepgo() {
-   const nome = document.querySelector('[name="nome"]').value;
-   const email = document.querySelector('[name="email"]').value;
-   const subject = document.querySelector('[name="subject"]').value;
-   const body= document.querySelector('[name="body"]').value;
-   
-
-   console.log(nome);
-   console.log(email);
+    
+    const nome = document.querySelector('[name="nome"]').value;
+    const email = document.querySelector('[name="email"]').value;
+    const assunto = document.querySelector('[name="subject"]').value;
+    const descricao= document.querySelector('[name="body"]').value; 
+    
+    console.log(nome);
 }
-
-/*
-const botao = document.querySelector('#meuBotao');
-botao.addEventListener('click', function() {
-   // Código a ser executado quando o botão é clicado
-   const nome = document.querySelector('[name="nome"]').value;
-   const email = document.querySelector('[name="email"]').value;
-   const subject = document.querySelector('[name="subject"]').value;
-   const body= document.querySelector('[name="body"]').value;
-
-   console.log(nome);
-   console.log(email);
-   console.log('Botão clicado!');
-});
-*/
